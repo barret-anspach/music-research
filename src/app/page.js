7c142f73c0bca9edb5e272726d957e1ac4bfc055
Barret Anspach <barret.anspach@gmail.com>
2023-08-14 19:19:55 -0700

    init

---
+++ src/app/page.js
@@ -0,0 +1,97 @@
+import styles from './page.module.css'
+import { BaseN, Combination, Permutation } from 'js-combinatorics';
+
+const seed = {
+  static: {
+    parts: 4,
+    polyphony: 4, // number of voices a "part" can play; here assumes all parts have same capabilities. should refactor so parts is [{polyphony: n}, …]
+  },
+  sets: {
+    pitches: ['A', 'B', 'C', 'D', 'E'],
+    timeSignatures: ['4/4', '6/8', '5/16'],
+    divisionsInMeasure: ['2', '4', '5'],
+  },
+};
+
+const generate = () => {
+  for (let key in seed.sets) {
+    console.log(seed.sets[key].length)
+  }
+  const pitches = new BaseN(seed.sets.pitches, seed.static.parts)
+  const timeSignatures = new BaseN(seed.sets.timeSignatures, seed.static.parts)
+  const tuplets = new BaseN(seed.sets.divisionsInMeasure, seed.static.parts)
+  console.log({ pitches, timeSignatures, tuplets })
+  // TODO: pitches.length % timeSignatures.length % tuplets.length
+  return {
+    pitches: [...pitches],
+    timeSignatures: [...timeSignatures],
+    tuplets: [...tuplets],
+  }
+}
+
+const seedResult = generate()
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.timeSignatureColumn}>
+        {seedResult.timeSignatures.map((tsigSet, index) => (
+          <div
+            key={`tsigset-${index}`}
+            className={styles.timeSignatureSet}
+          >
+            {tsigSet.map((tsig, tsigIndex) => (
+              <div
+                key={`tsigset-${index}-tsig-${tsigIndex}`}
+                className={styles.timeSignature}
+              >
+                {tsig.split('/').map((number, numberIndex) => (
+                  <div
+                    key={`tsigset-${index}-tsig-${tsigIndex}-${numberIndex === 0 ? 'numerator' : 'denominator'}`}
+                    className={numberIndex === 0 ? styles.timeSignatureNumerator : styles.timeSignatureDenominator}
+                  >
+                    {number}
+                  </div>
+                ))
+                }
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      <div>
+        {seedResult.pitches.map((pitchSet, index) => (
+          <div
+            className={styles.pitchSet}
+            key={`pitch-${index}`}
+          >
+            {pitchSet.map((pitch, playerIndex) => (
+              <div 
+                key={`pitch-${index}-player-${playerIndex}`} 
+                className={`${styles.pitch} ${styles[`pitch-${pitch}`]}`}
+              >
+                {pitch}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      <div>
+        {seedResult.tuplets.map((tupletSet, index) => (
+          <div
+            className={styles.tupletSet}
+            key={`tuplet-${index}`}
+          >
+            {tupletSet.map((tuplet, tupletIndex) => (
+              <div
+                key={`tupletSet-${index}-tuplet-${tupletIndex}`}
+                className={styles.tuplet}>
+                {tuplet}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </main>
+  )
+}
